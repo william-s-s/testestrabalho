143f0167c2bc662ceaa6f8da873928e18706567d
William Schmidt Soares <williamschmidtsoares@gmail.com>
2022-03-04 11:06:46 -0300

    Removido programa.exe de hello world e modificado intcounter

--- intcounter/main.c
+++ intcounter/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int random_int(int max){
+    srand(time(NULL));
     return rand() % max;
 }
 
-int main(int i){    
-    printf("%d", random_int(i));
+int main(int i){
+    int result = random_int(i);    
+    printf("%d", result);
     return 0;
 }
